51a048550c150f20bedebea2b0cc016da0a8634c
Nico Kuijlen <nico.kuijlen@nav.no>
2018-11-27 10:00:32 +0100

    Setup new project

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# dagpenger-metrics
-Common prometheus configuration for dagpenger
+# DAGPENGER-METRICS
+Common prometheus configuration for dagpenger.
+
+# Komme i gang
+
+Gradle brukes som byggverktøy og er bundlet inn.
+
+`./gradlew build`
+
+---
+
+# Henvendelser
+
+Spørsmål knyttet til koden eller prosjektet kan rettes mot:
+
+* André Roaldseth, andre.roaldseth@nav.no
+* Eller en annen måte for omverden å kontakte teamet på
+
+## For NAV-ansatte
+
+Interne henvendelser kan sendes via Slack i kanalen #dagpenger.
---
+++ build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    id("application")
+    kotlin("jvm") version "1.3.10"
+    id("com.diffplug.gradle.spotless") version "3.13.0"
+    id("com.palantir.docker") version "0.20.1"
+    id("com.palantir.git-version") version "0.11.0"
+    id("com.adarshr.test-logger") version "1.5.0"
+    id("info.solidsoft.pitest") version "1.3.0"
+}
+
+apply {
+    plugin("com.diffplug.gradle.spotless")
+    plugin("com.adarshr.test-logger")
+    plugin("info.solidsoft.pitest")
+}
+
+repositories {
+    jcenter()
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven(url = "http://packages.confluent.io/maven/")
+}
+
+val gitVersion: groovy.lang.Closure<Any> by extra
+version = gitVersion()
+group = "no.nav.dagpenger"
+
+application {
+    applicationName = "dagpenger-metrics"
+    mainClassName = "no.nav.dagpenger.metrics"
+}
+
+docker {
+    name = "repo.adeo.no:5443/navikt/${application.applicationName}"
+    buildArgs(
+        mapOf(
+            "APP_NAME" to application.applicationName,
+            "DIST_TAR" to "${application.applicationName}-${project.version}"
+        )
+    )
+    files(tasks.findByName("distTar")?.outputs)
+    pull(true)
+    tags(project.version.toString())
+}
+
+val kotlinLoggingVersion = "1.4.9"
+val fuelVersion = "1.15.0"
+val confluentVersion = "4.1.2"
+val kafkaVersion = "2.0.0"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("no.nav.dagpenger:streams:0.2.2-SNAPSHOT")
+    implementation("no.nav.dagpenger:events:0.1.9-SNAPSHOT")
+
+    implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
+    implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
+    implementation("com.github.kittinunf.fuel:fuel-gson:$fuelVersion")
+
+    compile("org.apache.kafka:kafka-clients:$kafkaVersion")
+    compile("org.apache.kafka:kafka-streams:$kafkaVersion")
+    compile("io.confluent:kafka-streams-avro-serde:$confluentVersion")
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("junit:junit:4.12")
+}
+
+spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts", "additionalScripts/*.gradle.kts")
+        ktlint()
+    }
+}
+
+pitest {
+    threads = 4
+    pitestVersion = "1.4.3"
+    coverageThreshold = 80
+    avoidCallsTo = setOf("kotlin.jvm.internal")
+    timestampedReports = false
+}
+
+tasks.getByName("check").dependsOn("pitest")
