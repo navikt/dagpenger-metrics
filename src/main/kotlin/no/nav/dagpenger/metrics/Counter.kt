b1b0d27b4d63a1d426e86608f4a075e8e7e3cf00
Nico Kuijlen <nico.kuijlen@nav.no>
2018-11-27 13:12:43 +0100

    Add helper functions for creating metric types

--- src/main/kotlin/no/nav/dagpenger/metrics/Counter.kt
+++ src/main/kotlin/no/nav/dagpenger/metrics/Counter.kt
@@ -1,11 +1,44 @@
 package no.nav.dagpenger.metrics
 
 import io.prometheus.client.Counter
+import io.prometheus.client.Gauge
+import io.prometheus.client.Histogram
+import io.prometheus.client.Summary
 
-fun aCounter(name: String, labelNames: List<String> = listOf(), helptext: String): io.prometheus.client.Counter {
-    return Counter.build()
-        .namespace("dagpenger")
-        .name(name)
-        .labelNames(labelNames)
-        .help(helptext).register()
+val DAGPENGER_NAMESPACE = "dagpenger"
+
+fun aCounter(name: String, labelNames: List<String> = listOf(), help: String): Counter {
+    return Counter
+        .build()
+        .namespace(DAGPENGER_NAMESPACE)
+        .help(help)
+        .labelNames(*labelNames.toTypedArray())
+        .register()
+}
+
+fun aGauge(name: String, labelNames: List<String> = listOf(), help: String): Gauge {
+    return Gauge
+        .build()
+        .namespace(DAGPENGER_NAMESPACE)
+        .help(help)
+        .labelNames(*labelNames.toTypedArray())
+        .register()
+}
+
+fun aSummary(name: String, labelNames: List<String> = listOf(), help: String): Summary {
+    return Summary
+        .build()
+        .namespace(DAGPENGER_NAMESPACE)
+        .help(help)
+        .labelNames(*labelNames.toTypedArray())
+        .register()
+}
+
+fun aHistogram(name: String, labelNames: List<String> = listOf(), help: String): Histogram {
+    return Histogram
+        .build()
+        .namespace(DAGPENGER_NAMESPACE)
+        .help(help)
+        .labelNames(*labelNames.toTypedArray())
+        .register()
 }
